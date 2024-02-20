2b7642b457f308f6672d741931b469c5770a2132
Duy1408 <duynguyen08142001@gmail.com>
2024-02-20 22:01:52 +0700

    CRUD Auction

--- DAO/AuctionDAO.cs
+++ DAO/AuctionDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessObject.BusinessObject;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,77 @@ namespace DAO
 {
     public class AuctionDAO
     {
+        private static AuctionDAO instance;
+
+        public static AuctionDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AuctionDAO();
+                }
+                return instance;
+            }
+        }
+
+        public List<Auction> GetAllAuction()
+        {
+            var _context = new TheRealEstateDBContext();
+            return _context.Auctions.Include(c => c.RealEstate).ToList();
+        }
+
+        public bool AddNewAuction(Auction auction)
+        {
+            var _context = new TheRealEstateDBContext();
+            var a = _context.Auctions.SingleOrDefault(c => c.AuctionID == auction.AuctionID);
+
+            if (a != null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Auctions.Add(auction);
+                _context.SaveChanges();
+                return true;
+
+            }
+        }
+
+        public bool UpdateAuction(Auction auction)
+        {
+            var _context = new TheRealEstateDBContext();
+            var a = _context.Auctions.SingleOrDefault(c => c.AuctionID == auction.AuctionID);
+
+            if (a == null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Entry(a).CurrentValues.SetValues(auction);
+                _context.SaveChanges();
+                return true;
+            }
+        }
+
+        public Auction GetAuctionByID(int id)
+        {
+            var _context = new TheRealEstateDBContext();
+            return _context.Auctions.SingleOrDefault(a => a.AuctionID == id);
+        }
+
+        public void DeleteAuction(Auction auction)
+        {
+            var _context = new TheRealEstateDBContext();
+
+            var a = _context.Auctions.FirstOrDefault(a => a.AuctionID == auction.AuctionID);
+            _context.Auctions.Remove(a);
+
+            _context.SaveChanges();
+        }
+
+       
     }
 }
---
+++ GroupProject/Controllers/AuctionController/AuctionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.BusinessObject;
+using Service.Interface;
+
+namespace GroupProject.Controllers.AuctionController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly IAuctionService _auction;
+
+        public AuctionsController(IAuctionService auction)
+        {
+            _auction = auction;
+        }
+
+        // GET: api/Auctions
+        [HttpGet]
+        public ActionResult<IEnumerable<Auction>> GetAuctions()
+        {
+          if (_auction.GetAuction() == null)
+          {
+              return NotFound();
+          }
+            return _auction.GetAuction().ToList()
+;        }
+
+        // GET: api/Auctions/5
+        [HttpGet("{id}")]
+        public ActionResult<Auction> GetAuction(int id)
+        {
+          if (_auction.GetAuction() == null)
+          {
+              return NotFound();
+          }
+            var auction =  _auction.GetAuctionById(id);
+
+            if (auction == null)
+            {
+                return NotFound();
+            }
+
+            return auction;
+        }
+
+        // PUT: api/Auctions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuction(int id, Auction auction)
+        {
+            if (_auction.GetAuction()==null)
+            {
+                return BadRequest();
+            }
+        
+
+            try
+            {
+                _auction.UpdateAuction(auction);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_auction.GetAuctionById(id)==null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Auctions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Auction>> PostAuction(Auction auction)
+        {
+          if (_auction.GetAuction()==null)
+          {
+              return Problem("Entity set 'TheRealEstateDBContext.Auctions'  is null.");
+          }
+            _auction.AddNewAuction(auction);
+
+            return CreatedAtAction("GetAuction", new { id = auction.AuctionID }, auction);
+        }
+
+        // DELETE: api/Auctions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuction(int id)
+        {
+            if (_auction.GetAuction()==null)
+            {
+                return NotFound();
+            }
+            var auction = _auction.GetAuctionById(id);
+            if (auction == null)
+            {
+                return NotFound();
+            }
+
+            _auction.DeleteAuction(auction);
+
+            return NoContent();
+        }
+
+     
+    }
+}
