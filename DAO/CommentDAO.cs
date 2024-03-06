b6a03f7f22fbd4d3e8de5de39271ad3b4af7f969
Duy1408 <duynguyen08142001@gmail.com>
2024-03-06 14:32:28 +0700

    comment

--- DAO/CommentDAO.cs
+++ DAO/CommentDAO.cs
@@ -27,11 +27,62 @@ namespace DAO
         public List<Comment> GetAllComment()
         {
             var _context = new TheRealEstateDBContext();
-            return _context.Comments.Include(c => c.UserID).Include(c => c.RealEstateID).ToList();
+            return _context.Comments.ToList();
 
         }
 
 
-  
+        public bool AddNewComment(Comment comment)
+        {
+            var _context = new TheRealEstateDBContext();
+            var a = _context.Comments.SingleOrDefault(c => c.CommentID == comment.CommentID);
+
+            if (a != null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+                return true;
+
+            }
+        }
+
+
+        public bool UpdateComment(Comment comment)
+        {
+            var _context = new TheRealEstateDBContext();
+            var a = _context.Comments.SingleOrDefault(c => c.CommentID == comment.CommentID);
+
+            if (a == null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Entry(a).CurrentValues.SetValues(comment);
+                _context.SaveChanges();
+                return true;
+            }
+        }
+
+        public Comment GetCommentByID(int id)
+        {
+            var _context = new TheRealEstateDBContext();
+            return _context.Comments.SingleOrDefault(a => a.CommentID == id);
+        }
+
+        public void DeleteComment(Comment comment)
+        {
+            var _context = new TheRealEstateDBContext();
+
+            var a = _context.Comments.FirstOrDefault(a => a.CommentID == comment.CommentID);
+            _context.Comments.Remove(a);
+
+            _context.SaveChanges();
+        }
+
     }
 }
