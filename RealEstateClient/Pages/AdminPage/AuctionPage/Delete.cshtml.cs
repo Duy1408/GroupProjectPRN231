411efb9b9e1ccac1ce27533f1eab773578400b1c
duongmake <duongmake3@gmail.com>
2024-03-14 14:12:16 +0700

    commit Delete Auction

--- RealEstateClient/Pages/AdminPage/AuctionPage/Delete.cshtml.cs
+++ RealEstateClient/Pages/AdminPage/AuctionPage/Delete.cshtml.cs
@@ -6,57 +6,61 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BusinessObject.BusinessObject;
+using System.Net.Http.Headers;
+using BusinessObject.DTO.Request;
+using System.Text.Json;
 
 namespace RealEstateClient.Pages.AdminPage.AuctionPage
 {
     public class DeleteModel : PageModel
     {
-        private readonly BusinessObject.BusinessObject.TheRealEstateDBContext _context;
-
-        public DeleteModel(BusinessObject.BusinessObject.TheRealEstateDBContext context)
+        private readonly HttpClient _httpClient;
+        private string ApiUrl = "";
+        public DeleteModel()
         {
-            _context = context;
+            _httpClient = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _httpClient.DefaultRequestHeaders.Accept.Add(contentType);
+            ApiUrl = "https://localhost:7088/api/Auctions";
         }
 
         [BindProperty]
-      public Auction Auction { get; set; } = default!;
+        public Auction Auction { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Auctions == null)
-            {
-                return NotFound();
-            }
+            HttpResponseMessage responseMessage = await _httpClient.GetAsync($"{ApiUrl}/{id}");
+            string strData = await responseMessage.Content.ReadAsStringAsync();
 
-            var auction = await _context.Auctions.FirstOrDefaultAsync(m => m.AuctionID == id);
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var _auction = JsonSerializer.Deserialize<Auction>(strData, options)!;
 
-            if (auction == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Auction = auction;
-            }
+
+            Auction = _auction;
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null || _context.Auctions == null)
+            try
             {
-                return NotFound();
+                HttpResponseMessage response = await _httpClient.DeleteAsync($"{ApiUrl}/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    ViewData["Success"] = "Delete Success";
+                    return RedirectToPage("./Index");
+                }
+                ViewData["Error"] = "Delete Error";
+                return RedirectToPage("./Index");
             }
-            var auction = await _context.Auctions.FindAsync(id);
-
-            if (auction != null)
+            catch
             {
-                Auction = auction;
-                _context.Auctions.Remove(Auction);
-                await _context.SaveChangesAsync();
+                ViewData["Error"] = "Fail To Call API";
+                return RedirectToPage("/Error");
             }
-
-            return RedirectToPage("./Index");
         }
     }
 }
